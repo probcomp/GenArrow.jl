4276d991220bd96000750ac2c08b3ac4d68d8026
Ian Limarta <ian.limarta@gmail.com>
2022-10-06 10:06:41 -0400

    Refactored. Metadata test

--- example/meta.jl
+++ example/meta.jl
@@ -11,16 +11,16 @@ using LinearAlgebra
 # Here, we define a model and a submodel.
 
 @gen function submodel()
-    for k in 1:100
-        {:y => k => k} ~ normal(0.0, 1.0)
-    end
+  for k in 1:100
+    {:y => k => k} ~ normal(0.0, 1.0)
+  end
 end
 
 @gen function model()
-    for k in 1:1000
-        {:x => k => k} ~ mvnormal(zeros(100), I(100))
-    end
-    q ~ submodel()
+  for k in 1:1000
+    {:x => k => k} ~ mvnormal(zeros(100), I(100))
+  end
+  q ~ submodel()
 end
 
 # To create a `GenArrow.jl` managed directory, we pass a path into
@@ -29,15 +29,15 @@ end
 # a management structure which provides interfaces to read/write serialized
 # output.
 activate(Path("./sample")) do ctx
-    # Here, we sample a `tr::Gen.Trace` for our model.
-    # Then, we save it to the serialization directory
-    # with `GenArrow.write!`
-    tr = simulate(model, ())
-    GenArrow.write!(tx, tr)
-    # `GenArrow` keeps track of each trace using a UUID.
+  # Here, we sample a `tr::Gen.Trace` for our model.
+  # Then, we save it to the serialization directory
+  # with `GenArrow.write!`
+  tr = simulate(model, ())
+  GenArrow.write!(ctx, tr; x=333, y=111, z=123)
+  # `GenArrow` keeps track of each trace using a UUID.
 
-    # Multiple `write!` statements are perfectly acceptable.
-    tr = simulate(model, ())
+  # Multiple `write!` statements are perfectly acceptable.
+  tr = simulate(model, ())
 end
 
 # Now, once we have a serialization directory, we may want to query it later,
