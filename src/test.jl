5a123980567976c8e23f2fcdea64ca4d58a29ad4
Ian Limarta <ian.limarta@gmail.com>
2022-11-28 12:22:50 -0500

    JLD2 testing

--- src/test.jl
+++ src/test.jl
@@ -1,24 +1,9 @@
-import Arrow
-using Query
-import FilePathsBase
-using DataFrames
+using JLD2
 
-d1 = (name=[1, 2, 3], age=[3, 4, 5])
-paths_file = FilePathsBase.Path("./src/test.arrow")
-open(paths_file, "w") do io
-  Arrow.write(io, d1)
+jldopen("example.jld2", "w") do f
+  f["file/what"] = 100
 end
+f = jldopen("example.jld2", "r")
+display(f)
+close(f)
 
-tbl = Tables.datavaluerows(Arrow.Table(paths_file))
-
-# x = @from(i in tbl, begin
-#   @select i
-# end)
-q = Meta.parse("begin @select i end")
-function foo(q)
-  println(typeof(q))
-end
-print(foo(q))
-# print(typeof(q))
-x = @from(i in tbl, q)
-# print(x)
