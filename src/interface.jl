d746b8440e0c1cb6ce66f3dd540b1405ee6420ed
Ian Limarta <ian.limarta@gmail.com>
2023-03-06 17:31:57 -0500

    PersistentVector needed in interface.jl. TODO: Resolve import/using usage

--- src/interface.jl
+++ src/interface.jl
@@ -1,4 +1,5 @@
 import .Serialization
+using FunctionalCollections
 
 mutable struct TraceIO <: IO
     io::IOBuffer
@@ -40,13 +41,13 @@ function serialize(tr::Gen.VectorTrace{Gen.MapType, U, V}) where {U,V}
     write(io, tr.score)
     write(io, tr.noise)
     println("SERIALIZE VECTOR TRACE")
-    println("args: ", tr.args)
+    # println("args: ", tr.args)
     println("retval: ", tr.retval)
-    println("subtraces len: ", length(tr.subtraces))
-    println("len: ", tr.len)
-    println("num_nonempty: ", tr.num_nonempty)
-    println("score: ", tr.score)
-    println("noise: ", tr.noise)
+    # println("subtraces len: ", length(tr.subtraces))
+    # println("len: ", tr.len)
+    # println("num_nonempty: ", tr.num_nonempty)
+    # println("score: ", tr.score)
+    # println("noise: ", tr.noise)
     println("END")
 
     for subtrace in tr.subtraces # TODO: Figure out if append type before helps
@@ -84,22 +85,22 @@ function deserialize(gen_fn, io, maptype::Type{Gen.VectorTrace{Gen.MapType, U, V
     score = read(io, Float64)
     noise = read(io, Float64)
     println("DESERIALIZE VECTOR TRACE")
-    println("args: ", args)
+    # println("args: ", args)
     println("retval: ", retval)
-    println("subtraces len: ", subtraces_count)
-    println("len: ", len)
-    println("num_nonempty: ", num_nonempty)
-    println("score: ", score)
-    println("noise: ", noise)
+    # println("subtraces len: ", subtraces_count)
+    # println("len: ", len)
+    # println("num_nonempty: ", num_nonempty)
+    # println("score: ", score)
+    # println("noise: ", noise)
     println("END")
     
-    subtraces = [] # Optimize type inference
+    subtraces = PersistentVector{Gen.DynamicDSLTrace}() # Optimize type inference
     # println(subtraces_count, " ", retval_count, " ", len, " ", num_nonempty, " ", noise)
     for i=1:subtraces_count
         type = Serialization.deserialize(io)
-        println("Deserialize type: ", type)
+        # println("Deserialize type: ", type)
         tr = GenArrow.deserialize(gen_fn.kernel, io, type)
-        push!(subtraces, tr)
+        subtraces = push(subtraces, tr)
     end
     maptype(gen_fn, subtraces, retval, args, score, noise, len, num_nonempty)
 end
